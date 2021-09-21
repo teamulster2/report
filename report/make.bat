1ebb4b77c34af45237f7404c6a96f86728bb1edf
mac641 <marcel.arndt1@icloud.com>
2021-09-21 16:44:29 +0200

    [issue/17] - Add sections
    for config manager, database manager, service provider and testing
    
      - [issue/20] - Add Makefile to build pdf with pandoc
      - [issue/20] - Add styles.md and disable image floating

--- report/make.bat
+++ report/make.bat
@@ -1,12 +1,12 @@
 :: quick workaround for windows users
-pandoc -s -o 00_abstract/report.md --defaults 00_abstract/includes.yaml
-pandoc -s -o 01_introduction/report.md --defaults 01_introduction/includes.yaml
-pandoc -s -o 02_background/report.md --defaults 02_background/includes.yaml
-pandoc -s -o 03_methodology/report.md --defaults 03_methodology/includes.yaml
-pandoc -s -o 04_implementation/report.md --defaults 04_implementation/includes.yaml
-pandoc -s -o 05_results_and_discussion/report.md --defaults 05_results_and_discussion/includes.yaml
-pandoc -s -o 06_conclusion/report.md --defaults 06_conclusion/includes.yaml
-pandoc -s -o 07_references/report.md --defaults 07_references/includes.yaml
+pandoc -o 00_abstract/report.md --standalone --defaults 00_abstract/includes.yaml -f markdown --pdf-engine=xelatex
+pandoc -o 01_introduction/report.md --standalone --defaults 01_introduction/includes.yaml -f markdown --pdf-engine=xelatex
+pandoc -o 02_background/report.md --standalone --defaults 02_background/includes.yaml -f markdown --pdf-engine=xelatex
+pandoc -o 03_methodology/report.md --standalone --defaults 03_methodology/includes.yaml -f markdown --pdf-engine=xelatex
+pandoc -o 04_implementation/report.md --standalone --defaults 04_implementation/includes.yaml -f markdown --pdf-engine=xelatex
+pandoc -o 05_results_and_discussion/report.md --standalone --defaults 05_results_and_discussion/includes.yaml -f markdown --pdf-engine=xelatex
+pandoc -o 06_conclusion/report.md --standalone --defaults 06_conclusion/includes.yaml -f markdown --pdf-engine=xelatex
+pandoc -o 07_references/report.md --standalone --defaults 07_references/includes.yaml -f markdown --pdf-engine=xelatex
 
-pandoc -s -o report.md --defaults includes.yaml
-pandoc -s -o report.pdf --defaults includes.yaml
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+pandoc styles.md -o report.pdf --standalone --defaults includes.yaml -f markdown -H disable_float.tex --pdf-engine=xelatex
---
+++ report/styles.md
@@ -0,0 +1,26 @@
+---
+title: soTired - Report
+author:
+  - Carina Strehler
+  - Anna Gsell
+  - Kilian Drechsler
+  - Lukas Diehm
+  - Marcel Arndt
+date: \today{}
+lang: en-GB
+documentclass: scrartcl
+mainfont: Arial
+sansfont: Arial
+monofont: Arial
+mathfont: Arial
+fontsize: 11pt
+header-includes: \usepackage{setspace}
+geometry: margin=2cm
+papersize: a4
+indent: false
+linestretch: 1.1
+links-as-notes: true
+urlcolor: NavyBlue
+toc: false
+# toc-depth: 3
+---
