c09ec17ddcef16665b271eeba06a9e1468b47288
carinaHSA <carina.strehler@hs-augsburg.de>
2021-09-22 11:44:49 +0200

    [issue/17] - Changed make.bat for windows

--- make.bat
+++ make.bat
@@ -1,24 +1,24 @@
 :: quick workaround for windows users
-	cd report/; \
-cd 00_abstract/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
-cd ../01_introduction/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
-cd ../02_background/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
-cd ../03_methodology/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
-cd ../04_implementation/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
-cd ../05_results_and_discussion/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
-cd ../06_conclusion/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
-cd ../07_references/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
+	cd report/
+cd 00_abstract/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+cd ../01_introduction/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+cd ../02_background/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+cd ../03_methodology/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+cd ../04_implementation/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+cd ../05_results_and_discussion/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+cd ../06_conclusion/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+cd ../07_references/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
 cd ../../
 
-	cd report/report-build/; \
-pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex; \
-pandoc styles.md -o ../../report.pdf --standalone --defaults includes.yaml -f markdown -H disable_float.tex --pdf-engine=xelatex; \
+	cd report/report-build/
+pandoc -o report.md --standalone --defaults includes.yaml -f markdown --pdf-engine=xelatex
+pandoc styles.md -o ../../report.pdf --standalone --defaults includes.yaml -f markdown -H disable_float.tex --pdf-engine=xelatex
 cd ../../
